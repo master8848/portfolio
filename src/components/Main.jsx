519bea12bef7c613e89f85059afd148835c8f12c
saurav sanjel <sauravsanjelgg@gmail.com>
2022-06-30 17:16:27 +0545

    makings things good and bad at same time

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div className="container">
+      <img src="../images/profile.jsx" alt="My picture" />
+    </div>
+  );
+}
+
+export default Main;
